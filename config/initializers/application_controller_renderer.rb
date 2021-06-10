65b6d2686eaa141924fffb9006db84eefd79b2ae
goro-cat <goro2.8223@gmail.com>
2021-06-10 12:50:22 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6ee5573e73858135574db472621ac44d0dfafc02f7f0e4f3a7458bff11a425c63fe0bd4f880210589ec725649145c1a058f3e0cde0e46c4bf000aaa8f49518d2'
